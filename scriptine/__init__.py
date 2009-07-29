f27c9d429f4828871caff97c836dbd252e37eaf0
olt@bogosoft.com <olt@bogosoft.com>
2009-07-29 11:40:23 +0200

    export run function instead of parse_and_run_commands

--- scriptine/__init__.py
+++ scriptine/__init__.py
@@ -1,6 +1,6 @@
 from scriptine.path import path
-from scriptine.command import parse_and_run_commands, global_options
+from scriptine.command import run, global_options
 
 __version__ = '0.0.6'
 
-__all__ = ['path', 'parse_and_run_commands']
+__all__ = ['path', 'run']
