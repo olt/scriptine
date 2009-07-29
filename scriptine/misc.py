c42de82fadb7fe1505579ac35e95bceeed351ddc
olt@bogosoft.com <olt@bogosoft.com>
2009-07-29 17:40:10 +0200

    use decorator.decorator instead of simple_decorator to keep function signatures

--- scriptine/misc.py
+++ scriptine/misc.py
@@ -1,3 +1,6 @@
+import sys
+import re
+import inspect
 from distutils.core import Command
 from scriptine import log
 
@@ -36,28 +39,131 @@ class Options(dict):
 options = Options()
 options.dry = False
 
-def simple_decorator(decorator):
-    """This decorator can be used to turn simple functions
-    into well-behaved decorators, so long as the decorators
-    are fairly simple. If a decorator expects a function and
-    returns a function (no descriptors), and if it doesn't
-    modify function attributes or docstring, then it is
-    eligible to use this. Simply apply @simple_decorator to
-    your decorator and it will automatically preserve the
-    docstring and function attributes of functions to which
-    it is applied."""
-    def new_decorator(f):
-        g = decorator(f)
-        g.__name__ = f.__name__
-        g.__doc__ = f.__doc__
-        g.__dict__.update(f.__dict__)
-        return g
-    # Now a few lines needed to make simple_decorator itself
-    # be a well-behaved decorator.
-    new_decorator.__name__ = decorator.__name__
-    new_decorator.__doc__ = decorator.__doc__
-    new_decorator.__dict__.update(decorator.__dict__)
-    return new_decorator
+
+# --- begin decorator ---
+##########################     LICENCE     ###############################
+      
+##   Redistributions of source code must retain the above copyright 
+##   notice, this list of conditions and the following disclaimer.
+##   Redistributions in bytecode form must reproduce the above copyright
+##   notice, this list of conditions and the following disclaimer in
+##   the documentation and/or other materials provided with the
+##   distribution. 
+
+##   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+##   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+##   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+##   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+##   HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+##   INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+##   BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+##   OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+##   ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+##   TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+##   USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+##   DAMAGE.
+
+"""
+Decorator module, see http://pypi.python.org/pypi/decorator
+for the documentation.
+"""
+
+# basic functionality
+class FunctionMaker(object):
+    """
+    An object with the ability to create functions with a given signature.
+    It has attributes name, doc, module, signature, defaults, dict and
+    methods update and make.
+    """
+    DEF = re.compile('\s*def\s*([_\w][_\w\d]*)\s*\(')
+    def __init__(self, func=None, name=None, signature=None,
+                 defaults=None, doc=None, module=None, funcdict=None):
+        if func:
+            # func can also be a class or a callable, but not an instance method
+            self.name = func.__name__
+            if self.name == '<lambda>': # small hack for lambda functions
+                self.name = '_lambda_' 
+            self.doc = func.__doc__
+            self.module = func.__module__
+            if inspect.isfunction(func):
+                self.signature = inspect.formatargspec(
+                    formatvalue=lambda val: "", *inspect.getargspec(func))[1:-1]
+                self.defaults = func.func_defaults
+                self.dict = func.__dict__.copy()
+        if name:
+            self.name = name
+        if signature is not None:
+            self.signature = signature
+        if defaults:
+            self.defaults = defaults
+        if doc:
+            self.doc = doc
+        if module:
+            self.module = module
+        if funcdict:
+            self.dict = funcdict
+        # check existence required attributes
+        assert hasattr(self, 'name')
+        if not hasattr(self, 'signature'):
+            raise TypeError('You are decorating a non function: %s' % func)
+
+    def update(self, func, **kw):
+        "Update the signature of func with the data in self"
+        func.__name__ = self.name
+        func.__doc__ = getattr(self, 'doc', None)
+        func.__dict__ = getattr(self, 'dict', {})
+        func.func_defaults = getattr(self, 'defaults', None)
+        callermodule = sys._getframe(3).f_globals.get('__name__', '?')
+        func.__module__ = getattr(self, 'module', callermodule)
+        func.__dict__.update(kw)
+ 
+    def make(self, src_templ, evaldict=None, addsource=False, **attrs):
+        "Make a new function from a given template and update the signature"
+        src = src_templ % vars(self) # expand name and signature
+        evaldict = evaldict or {}
+        mo = FunctionMaker.DEF.match(src)
+        if mo is None:
+            raise SyntaxError('not a valid function template\n%s' % src)
+        name = mo.group(1) # extract the function name
+        reserved_names = set([name] + [
+            arg.strip(' *') for arg in self.signature.split(',')])
+        for n, v in evaldict.iteritems():
+            if n in reserved_names:
+                raise NameError('%s is overridden in\n%s' % (n, src))
+        if not src.endswith('\n'): # add a newline just for safety
+            src += '\n'
+        try:
+            code = compile(src, '<string>', 'single')
+            exec code in evaldict
+        except:
+            print >> sys.stderr, 'Error in generated code:'
+            print >> sys.stderr, src
+            raise
+        func = evaldict[name]
+        if addsource:
+            attrs['__source__'] = src
+        self.update(func, **attrs)
+        return func
+
+def decorator(caller, func=None):
+    """
+    decorator(caller) converts a caller function into a decorator;
+    decorator(caller, func) decorates a function using a caller.
+    """
+    if func is None: # returns a decorator
+        fun = FunctionMaker(caller)
+        first_arg = inspect.getargspec(caller)[0][0]
+        src = 'def %s(%s): return _call_(caller, %s)' % (
+            caller.__name__, first_arg, first_arg)
+        return fun.make(src, dict(caller=caller, _call_=decorator),
+                        undecorated=caller)
+    else: # returns a decorated function
+        fun = FunctionMaker(func)
+        src = """def %(name)s(%(signature)s):
+    return _call_(_func_, %(signature)s)"""
+        return fun.make(src, dict(_func_=func, _call_=caller), undecorated=func)
+
+# --- end decorator ---
 
 def dry(message, func, *args, **kw):
     log.info(message)
@@ -65,15 +171,13 @@ def dry(message, func, *args, **kw):
     if not options.dry:
         return func(*args, **kw)
 
-@simple_decorator
-def log_call(func):
-    def _log_call(*args, **kw):
-        _log_function_call(func, *args, **kw)
-        return func(*args, **kw)
-    return _log_call
+@decorator
+def log_call(func, *args, **kw):
+    _log_function_call(func, *args, **kw)
+    return func(*args, **kw)
 
 def _log_function_call(func, *args, **kw):
-    message = func.func_name
+    message = func.__name__
     if args:
         message += ' ' + ' '.join(map(str, args))
     if kw:
@@ -81,10 +185,8 @@ def _log_function_call(func, *args, **kw):
         message += '(' + kw_str + ')'
     log.info(message)
 
-@simple_decorator
-def dry_guard(func):
-    def _dry_guard(*args, **kw):
-        _log_function_call(func, *args, **kw)
-        if not options.dry:
-            return func(*args, **kw)
-    return _dry_guard
+@decorator
+def dry_guard(func, *args, **kw):
+    _log_function_call(func, *args, **kw)
+    if not options.dry:
+        return func(*args, **kw)
