1bf1cff3e23518a002dc2da9d53964829e4a46ae
olt@bogosoft.com <olt@bogosoft.com>
2009-08-04 17:38:59 +0200

    add some simple tests for scriptine.command

---
+++ test/test_command.py
@@ -0,0 +1,69 @@
+from __future__ import with_statement
+from scriptine import run
+from contextlib import contextmanager
+try:
+    from cStringIO import StringIO
+except ImportError:
+    from StringIO import StringIO
+
+import sys
+class SysExitError(Exception):
+    pass
+
+_sys_exit = sys.exit
+
+def setup_module():
+    def mock_exit(status):
+        raise SysExitError()
+    sys.exit = mock_exit
+
+def teardown_module():
+    sys.exit = _sys_exit
+
+state = set()
+
+@contextmanager
+def stdout_capture():
+    orig_stdout, orig_stderr = sys.stdout, sys.stderr
+    sys.stderr.flush()
+    sys.stdout.flush()
+    
+    sys.stdout, sys.stderr = StringIO(), StringIO()
+    
+    try:
+        yield sys.stdout, sys.stderr
+    finally:
+        sys.stderr.flush()
+        sys.stdout.flush()
+        sys.stdout, sys.stderr = orig_stdout, orig_stderr
+    
+
+def foo1_command():
+    state.add('foo1_command_called')
+
+def foo2_command(required_arg):
+    state.add('foo2_command_called')
+    state.add('foo2_command_called_with_' + required_arg)
+
+def test_foo1():
+    run(args=['test_command.py', 'foo1'])
+    assert 'foo1_command_called' in state
+
+def test_foo2():
+    with stdout_capture() as (stdout, stderr):
+        try:
+            run(args=['test_command.py', 'foo2'])
+        except SysExitError:
+            pass
+        else:
+            assert False, 'did not exit'
+        stderr.seek(0)
+        out = stderr.read()
+        assert 'number of arguments does not match' in out
+    assert 'foo2_command_called' not in state
+
+def test_foo2_w_arg():
+    run(args=['test_command.py', 'foo2', 'buzz'])
+    assert 'foo2_command_called_with_buzz' in state
+
+    
